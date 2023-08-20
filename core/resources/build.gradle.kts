e0317c793728695421fabaf97755c58f9145d3a9
Bello Abraham <belloabraham101@gmail.com>
2023-08-20 17:21:42 +0100

    Added resources module to project

---
+++ core/resources/build.gradle.kts
@@ -0,0 +1,10 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "org.cccsharonparish.core.resources"
+    compileSdk = 34
+}
