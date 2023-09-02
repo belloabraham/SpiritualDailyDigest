5a48c5d0118cc10dc3d0bd7f63bf64ec00957dd5
Bello Abraham <belloabraham101@gmail.com>
2023-09-02 18:47:27 +0100

    Work in progress - Implementing Logger Util

---
+++ plugins/convention/src/main/java/org/cccsharonparish/plugins/convention/AppModulePlugin.kt
@@ -0,0 +1,47 @@
+package org.cccsharonparish.plugins.convention
+
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AppModulePlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target){
+            with(pluginManager) {
+                apply( "com.android.application")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.cccsharonparish.compose")
+            }
+
+            extensions.configure<ApplicationExtension>{
+                configureAndroidAndKotlin(this)
+
+                defaultConfig{
+                    targetSdk = Config.TARGET_SDK
+                    multiDexEnabled = true
+                }
+
+                buildTypes {
+                    debug {
+                        isDebuggable = true
+                    }
+                    release {
+                        isDebuggable = false
+                        isShrinkResources = true
+                        multiDexEnabled = true
+                    }
+                }
+            }
+
+            dependencies{
+                "implementation"(libs["core.splashscreen"])
+                "implementation"(libs["accompanist.systemuicontroller"])
+                "debugImplementation"(libs["leakcanary.android"])
+            }
+
+        }
+    }
+}
