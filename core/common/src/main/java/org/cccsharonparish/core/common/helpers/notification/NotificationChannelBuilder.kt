3a3b9c26a29dcf762f2a264f5a633cd12e87f1ca
Bello Abraham <belloabraham101@gmail.com>
2023-09-28 08:12:14 +0100

    Implemented notification

---
+++ core/common/src/main/java/org/cccsharonparish/core/common/helpers/notification/NotificationChannelBuilder.kt
@@ -0,0 +1,44 @@
+package org.cccsharonparish.core.common.helpers.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class NotificationChannelBuilder(
+    private val context: Context,
+) {
+
+    fun createNotificationChannels(
+        notificationDescription:String,
+        notificationChannelId:String,
+        colorRes:Int
+    ){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                notificationChannelId, notificationDescription,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channelSettings(notificationChannel, notificationDescription, colorRes)
+            val nManager = context.getSystemService(
+                NotificationManager::class.java
+            )!!
+            nManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun channelSettings(nChannel: NotificationChannel, description: String, colorRes:Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            nChannel.enableLights(true)
+            nChannel.lightColor = context.getColor(colorRes)
+            nChannel.enableVibration(true)
+            nChannel.setShowBadge(true)
+            nChannel.description = description
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                nChannel.setAllowBubbles(true)
+            }
+        }
+    }
+
+
+}
