3f8ffc8385598e30177b6929212eb1aa17ab92d9
Bello Abraham <belloabraham101@gmail.com>
2023-10-02 18:55:29 +0100

    WIP - Implementing Onboarding screen

--- feature/onboarding/src/main/java/dev/bellab/feature/onboarding/OnboardingActivity.kt
+++ feature/onboarding/src/main/java/dev/bellab/feature/onboarding/OnboardingActivity.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.bellab.feature.onboarding.ui.OnboardingPage
 import org.cccsharonparish.core.common.helpers.preference.ISettings
 import org.cccsharonparish.core.resources.ui.theme.SpiritualDailyDigestTheme
 import javax.inject.Inject
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dev.bellab.core.ui.PageIndicatorView
 import dev.bellab.feature.onboarding.ui.OnboardingUIState
+import kotlinx.coroutines.launch
 import org.cccsharonparish.core.resources.R
+import org.cccsharonparish.core.resources.ui.Size
+import org.cccsharonparish.core.resources.ui.theme.primary50
+import org.cccsharonparish.core.resources.ui.theme.primary600
 
 
 class OnboardingActivity : ComponentActivity() {
@@ -26,32 +41,90 @@ class OnboardingActivity : ComponentActivity() {
     @Inject
     lateinit var settings: ISettings
 
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         installSplashScreen()
 
-        super.onCreate(savedInstanceState)
         val onBackPressedCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
             }
         }
+
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
 
         setContent {
             val windowSizeClass = calculateWindowSizeClass(activity = this)
+            val scope = rememberCoroutineScope()
+            val pagerState = rememberPagerState(pageCount = {
+                4
+            })
             SpiritualDailyDigestTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                  OnboardingPage(windowSizeClass = windowSizeClass,
-                      uiState = OnboardingUIState(
-                      title="Title", subTitle = "Hello", description = "Lorem Ipsum", imageRes = R.drawable.large_notification_icon,
-                  ), {}, {}, {})
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        HorizontalPager(
+                            state = pagerState,
+                            modifier = Modifier.fillMaxSize(),
+                            beyondBoundsPageCount = pagerState.pageCount-1,
+                            ) { page ->
+                            OnboardingPage(
+                                currentPage = page,
+                                nextPage = page.plus(1),
+                                totalPage = pagerState.pageCount,
+                                windowSizeClass = windowSizeClass,
+                                uiState = OnboardingUIState(
+                                    title = "Title",
+                                    subTitle = "Hello",
+                                    description = "Lorem Ipsum",
+                                    imageRes = R.drawable.large_notification_icon,
+                                ),
+                                onSkip = {
+                                    scope.launch {
+                                        closeOnboardingActivity()
+                                    }
+                                }, onNext = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(page.plus(1))
+                                    }
+                                }, onPrev = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(page.minus(1))
+                                    }
+                                },
+                            )
+                        }
+                        Row(
+                            modifier = Modifier
+                                .padding(Size.MarginPaddingMedium(windowSizeClass))
+                                .align(Alignment.TopEnd),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        ) {
+                            for (i in 0 until pagerState.pageCount) {
+                                PageIndicatorView(
+                                    isSelected = i == pagerState.currentPage,
+                                    selectedColor = primary600,
+                                    defaultColor = primary50,
+                                    defaultRadius = 10.dp,
+                                    selectedLength = 20.dp,
+                                    animationDurationInMillis = 400,
+                                )
+                            }
+                        }
+                    }
+
                 }
             }
         }
+    }
+
+    private suspend fun closeOnboardingActivity() {
+        // settings.setBoolean(Key.USER_EXITED_ONBOARDING, true)
+        this@OnboardingActivity.finish()
     }
 }
 
--- feature/onboarding/src/main/java/dev/bellab/feature/onboarding/ui/Onboarding.kt
+++ feature/onboarding/src/main/java/dev/bellab/feature/onboarding/ui/Onboarding.kt
@@ -1,180 +1,46 @@
 package dev.bellab.feature.onboarding.ui
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import org.cccsharonparish.core.resources.R
 import org.cccsharonparish.core.resources.ui.Size
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnboardingPage(
+    currentPage: Int,
+    nextPage: Int,
+    totalPage: Int,
     windowSizeClass: WindowSizeClass,
     uiState: OnboardingUIState,
-    onSkip: () -> Unit, onNext: () -> Unit, onPrev: () -> Unit
+    onSkip: () -> Unit, onNext: () -> Unit, onPrev: () -> Unit,
 ) {
     Column(
-        Modifier.padding(16.dp),
+        Modifier
+            .padding(Size.MarginPaddingMedium(windowSizeClass))
+            .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        OnboardingHeader(uiState.title, uiState.subTitle)
+        OnboardingHeader(uiState.title, uiState.subTitle, windowSizeClass)
         OnboardingImage(
             uiState.description,
             uiState.imageRes,
             windowSizeClass
         )
         OnboardingFooter(
-            windowSizeClass, 
-            onSkip = onSkip, 
-            onNext =  onNext, 
-            onPrev = onPrev
+            currentPage = currentPage,
+            nextPage = nextPage,
+            totalPage = totalPage,
+            windowSizeClass,
+            onSkip = onSkip,
+            onNext = onNext,
+            onPrev = onPrev,
         )
-//        GetStartedFooter{
-//
-//        }
-    }
-}
-
-@Composable
-fun GetStartedFooter(onClick: () -> Unit) {
-    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
-        Button(
-            onClick = {
-                onClick()
-            },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = MaterialTheme.colors.secondary,
-            ),
-            modifier = Modifier
-                .fillMaxWidth(.8f)
-                .height(Size.ButtonHeigth())
-        ) {
-            Text(text = stringResource(id = R.string.lets_get_started), fontSize = 16.sp)
-        }
-    }
-}
-
-@Composable
-fun OnboardingHeader(
-    title:String,
-    subTitle:String
-) {
-    Row(
-        Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Text(text = title, style = MaterialTheme.typography.h4)
-            Text(text = subTitle, style = MaterialTheme.typography.subtitle2)
-        }
-        Column {
-            Text(text = "Dot Bubble")
-        }
-    }
-}
-
-@Composable
-fun OnboardingImage(
-    description: String,
-    @DrawableRes imageRes: Int,
-    windowSizeClass: WindowSizeClass
-) {
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .padding(vertical = Size.MarginPaddingMedium(windowSizeClass)),
-        verticalArrangement = Arrangement.spacedBy(Size.MarginPaddingMedium(windowSizeClass))
-    ) {
-        Image(
-            painter = painterResource(id = imageRes),
-            contentDescription = description,
-            Modifier
-                .width(Size.OnboardingImage(windowSizeClass))
-                .height(Size.OnboardingImage(windowSizeClass))
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(text = description, style = MaterialTheme.typography.body1)
-    }
-}
-
-@Composable
-fun OnboardingFooter(
-    windowSizeClass: WindowSizeClass,
-    onSkip: () -> Unit, onNext: () -> Unit, onPrev: () -> Unit
-) {
-    Row(
-        Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Row {
-            TextButton(
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Transparent,
-                    contentColor = MaterialTheme.colors.secondary
-                ),
-                onClick = {
-                    onSkip()
-                }
-            ) {
-                Text(text = stringResource(id = R.string.skip), style = MaterialTheme.typography.button)
-            }
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(Size.MarginPaddingSmall(windowSizeClass))
-        ) {
-
-            IconButton(
-                onClick = {
-                    onPrev()
-                },
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.KeyboardArrowLeft,
-                    contentDescription = stringResource(id = R.string.back_button),
-                )
-            }
-            TextButton(
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Transparent,
-                    contentColor = MaterialTheme.colors.secondary
-                ),
-                onClick = {
-                    onNext()
-                }
-            ) {
-                Text(text = stringResource(id = R.string.next), 
-                    style = MaterialTheme.typography.button
-                )
-            }
-        }
-
     }
 }
